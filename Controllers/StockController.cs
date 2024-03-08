3bf98136d172c9a52bccf0e0917fbe15f83876bc
Enzo Bozzani <bozzanienzo@gmail.com>
2024-03-08 11:27:11 -0300

    feat: development of stock PUT endpoint

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -50,5 +50,54 @@ namespace api.Controllers
             //irá executar o GetStockById, passando o Id e retornando no formato StockDto
             return CreatedAtAction(nameof(GetStockById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateStock([FromRoute] Guid id, [FromBody] UpdateStockDto updateStock)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            bool allFieldsAreNull = updateStock.Symbol == null && updateStock.CompanyName == null && updateStock.Industry == null && updateStock.Purchase == null && updateStock.LastDiv == null && updateStock.MarketCap == null;
+
+            if (allFieldsAreNull)
+            {
+                return BadRequest(new { error = "All fields are null! " });
+            }
+
+            if (updateStock.Symbol != null)
+            {
+                stock.Symbol = updateStock.Symbol;
+            }
+            if (updateStock.CompanyName != null)
+            {
+                stock.CompanyName = updateStock.CompanyName;
+            }
+            if (updateStock.Industry != null)
+            {
+                stock.Industry = updateStock.Industry;
+            }
+            if (updateStock.Purchase != null)
+            {
+                stock.Purchase = (decimal)updateStock.Purchase;
+            }
+            if (updateStock.LastDiv != null)
+            {
+                stock.LastDiv = (decimal)updateStock.LastDiv;
+            }
+            if (updateStock.MarketCap != null)
+            {
+                stock.MarketCap = (long)updateStock.MarketCap;
+            }
+
+            _context.SaveChanges();
+
+            return Ok(stock.ToStockDto());
+
+        }
     }
 }
