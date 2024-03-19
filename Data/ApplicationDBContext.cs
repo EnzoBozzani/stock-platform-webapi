1a6feeb89d3951bd61b6bcc4971243c4d3f91406
Enzo Bozzani <bozzanienzo@gmail.com>
2024-03-19 10:32:56 -0300

    feat: development of register controller

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,26 @@ namespace api.Data
         }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
